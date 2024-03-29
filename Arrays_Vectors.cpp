dce04d097caea52341c5a56ff11cdff51663d470
DeltaNaught <w0099673@umail.usq.edu.au>
2024-03-29 13:31:14 +1000

    Add files via upload

---
+++ Arrays_Vectors.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char argv[])
+{
+
+	vector <int> vector1, vector2;
+
+	vector1.push_back(1000);
+	vector1.push_back(20);
+
+	cout << vector1.at(0) << endl;
+	cout << vector1.at(1) << endl;
+	cout << vector1.size() << endl;
+
+	vector2.push_back(100);
+	vector2.push_back(200);
+
+	cout << endl << vector2.at(0) << endl;
+	cout << vector2.at(1) << endl;
+	cout << vector2.size() << endl;
+	
+	vector <vector <int>> vector_2d;
+
+	vector_2d.push_back(vector1);
+	vector_2d.push_back(vector2);
+
+	cout << vector_2d.at(0).at(0) << endl;
+	cout << vector_2d.at(0).at(1) << endl;
+	cout << vector_2d.at(1).at(0) << endl;
+	cout << vector_2d.at(1).at(1) << endl;
+
+	vector1.at(0) = 1000;
+
+	cout << endl << vector_2d.at(0).at(0) << endl;
+	cout << vector_2d.at(0).at(1) << endl;
+	cout << vector_2d.at(1).at(0) << endl;
+	cout << vector_2d.at(1).at(1) << endl;
+
+
+
+	cout << endl << vector1.at(0) << endl;
+	cout << vector1.at(1) << endl;
+
+	return 0;
+}
